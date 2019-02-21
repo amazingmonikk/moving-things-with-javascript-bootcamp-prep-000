a6bf2b4e45a040b10c6d68c1d270d0656030423a
Monika Tadross <monikk.tadross@gmail.com>
2019-02-21 19:08:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+function getIt() {
+ $('p').on("click", function(){
+    
+}); 
+}
+
+
+function frameIt() {
+   $('.tasty').on("load", function(){
+    
+});
+}
+
+
+function pressIt() {
+  
+}
+
+
+
+function submitIt() {
+  
+}
+
